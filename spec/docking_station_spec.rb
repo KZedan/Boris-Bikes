93dd2f9bb82dd2a6e2653828ce5e402b03665940
Loïc de Wit <dew.loic@gmail.com>
2019-06-25 16:21:52 +0100

    respond_to release_bike unit passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,10 @@
-require 'docking_station.rb'
+require './lib/docking_station'
+
 describe DockingStation do
+  describe 'release bike' do
+    it 'release bike' do
+      instance = DockingStation.new
+      expect(instance).to respond_to(:release_bike)
+    end
+  end
 end
