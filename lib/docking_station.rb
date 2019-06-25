e09111de6d39e678b301bfc2d8b7847fbbbb40dc
Kareem <kareem335@yahoo.co.uk>
2019-06-25 22:19:30 +0100

    modified rspec and lib

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,9 @@
+class Bike
+  def working?
+  end
+end
+
 class DockingStation
   def release_bike
-    return Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,7 @@
-require './lib/bike'
+require './lib/docking_station'
 
 describe DockingStation do
-  describe 'docking_station' do
-    it 'responds to release_bike' do
-      instance = DockingStation.new
-      expect(instance).to respond_to(:release_bike)
-    end
-  end
-  describe 'release_bike' do
-    it 'returns new bike' do
-      expect(release_bike).to eq Bike.new
-    end
+  it 'responds to release_bike' do
+    expect(subject).to respond_to :release_bike
   end
 end
